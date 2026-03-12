c5ebd4309499ca8c0d024209d618b82905fb0249
compscitwilight <compscitwilight@gmail.com>
2026-03-11 22:48:54 -0500

    create browse page and browse/color page

---
+++ src/app/browse/page.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function BrowsePage() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">Browse ponies.fyi</h1>
+            <i>Not to be confused with <Link className="text-sky-600 underline" href="/">search</Link></i>
+            <hr className="h-px my-2 border-0 bg-gray-400/50" />
+            <div className="p-2 grid gap-2">
+                <div className="flex gap-4 p-2 border border-gray-400/50 rounded-md">
+                    <div className="flex-1">
+                        <Link
+                            className="text-xl font-bold text-sky-600 underline"
+                            href="/browse/colors"
+                        >Browse ponysonas by color</Link>
+                        <p className="text-lg">
+                            Find ponysonas by the colors of their physical attributes
+                        </p>
+                    </div>
+                    <img className="border border-gray-400/50 p-1 object-fit w-[394px]" src="/browse-ponysonas-by-color.webp" />
+                </div>
+            </div>
+        </div>
+    )
+}
