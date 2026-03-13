24abfd14d5ef0c22706aca3b321671481ea81223
compscitwilight <compscitwilight@gmail.com>
2026-03-13 12:54:49 -0500

    make oauth redirectTo dynamic

--- src/app/pages/auth/page.tsx
+++ src/app/pages/auth/page.tsx
@@ -15,7 +15,7 @@ export default function AuthPage() {
         const { error } = await supabase.auth.signInWithOAuth({
             provider: "discord",
             options: {
-                redirectTo: `${process.env.NEXT_PUBLIC_APP_ENV === "production" ? "https://ponies.fyi" : "http://localhost:3000"}/api/auth/callback?next=/`
+                redirectTo: `${window.location.protocol}//${window.location.host}/api/auth/callback?next=/`
             }
         });
 
