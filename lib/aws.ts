754d79b3c2585f5975c0ddb1cd26fd0f05786228
compscitwilight <compscitwilight@gmail.com>
2026-03-14 22:21:08 -0500

    add logic for moveObject function

--- lib/aws.ts
+++ lib/aws.ts
@@ -1,4 +1,4 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, CopyObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3 = new S3Client({
@@ -7,20 +7,15 @@ const s3 = new S3Client({
     credentials: {
         accessKeyId: process.env.ACCESS_KEY_ID as string,
         secretAccessKey: process.env.SECRET_ACCESS_KEY as string
-    },
-    // signatureVersion: "v4"
+    }
 });
 
 /**
  * Generates a presigned PUT URL for user uploads directly to the configured S3 bucket.
  * @param mediaObjectUUID UUID of the existent Media row in the database
- * @param mime Content-type of the object being uploaded
  * @returns Direct R2 presigned PUT URL
  */
-export async function generatePresignedUploadURL(
-    mediaObjectUUID: string,
-    mime: string
-) {
+export async function generatePresignedUploadURL(mediaObjectUUID: string) {
     const command = new PutObjectCommand({
         Bucket: process.env.BUCKET_NAME,
         Key: mediaObjectUUID
@@ -34,5 +29,18 @@ export async function generatePresignedUploadURL(
 }
 
 export async function moveObject(source: string, destination: string) {
-    
+    const copyCommand = new CopyObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        CopySource: `${process.env.BUCKET_NAME}/${source}`,
+        Key: destination
+    }); 
+
+    await s3.send(copyCommand);
+
+    const deleteCommand = new DeleteObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: source
+    });
+
+    await s3.send(deleteCommand);
 }
